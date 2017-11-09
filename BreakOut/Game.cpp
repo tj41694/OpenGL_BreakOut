653955d13a79958f4eaaaedb6db0602d6f4efe45
tj41694 <tj41694@users.noreply.github.com>
2017-11-09 14:56:01 +0800

    2017-11-9

---
+++ BreakOut/Game.cpp
@@ -0,0 +1,29 @@
+#include "Game.h"
+
+
+
+Game::Game(GLuint width, GLuint height)
+	:State(GAME_ACTIVE), Keys(), Width(width), Height(height)
+{
+}
+
+
+Game::~Game()
+{
+}
+
+void Game::Init()
+{
+}
+
+void Game::ProcessInput(GLfloat dt)
+{
+}
+
+void Game::Update(GLfloat dt)
+{
+}
+
+void Game::Render()
+{
+}
